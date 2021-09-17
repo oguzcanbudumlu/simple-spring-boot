095b76829b1aee18c2fba3319eb588ee99ebfcf5
Oğuzcan Budumlu <obudumlu@gmail.com>
2021-09-17 15:50:44 +0300

    random book generator added

--- src/main/java/com/oguzcanbudumlu/simplespringboot/persistence/model/Book.java
+++ src/main/java/com/oguzcanbudumlu/simplespringboot/persistence/model/Book.java
@@ -1,11 +1,14 @@
 package com.oguzcanbudumlu.simplespringboot.persistence.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
+import org.apache.commons.lang3.RandomStringUtils;
 
 @Entity
 @Getter
+@Setter
 public class Book {
 
     @Id
@@ -17,4 +20,12 @@ public class Book {
 
     @Column(nullable = false)
     private String author;
+
+
+    public static Book randomGenerate() {
+        Book book = new Book();
+        book.setTitle(RandomStringUtils.randomAlphabetic(10));
+        book.setAuthor(RandomStringUtils.randomAlphabetic(15));
+        return book;
+    }
 }
